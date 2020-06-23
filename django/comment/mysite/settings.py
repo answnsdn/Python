8ee60f959722fbec6cbe283d05b04b3a44b62a76
answnsdn <answnsdn@gmail.com>
2020-06-23 17:44:27 +0900

    로그인

--- django/comment/mysite/settings.py
+++ django/comment/mysite/settings.py
@@ -31,8 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'bootstrap4',
     'articles',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
